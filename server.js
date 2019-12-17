e12ef24b1d243ee04c2020c2909402d8dce0c3cd
fainapahanko <fainapoganko@MacBook-Air-Faina.local>
2019-12-17 12:34:09 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const server = express()
+const booksRoute = require("./src/books/index")
+const cors = require("cors")
+require('dotenv').config()
+
+const port = process.env.PORT || 4000
+
+server.use(express.json())
+
+var whitelist = ['http://localhost:3000', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+server.use("/books", cors(corsOptions), booksRoute)
+
+
+server.listen(port,() => {
+    console.log(`We are running on localhost ${port}`)
+})
---
+++ src/books/index.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const fs = require("fs-extra")
+const router = express.Router()
+const path = require("path")
+const { check, validationResult, sanitizeBody } = require("express-validator")
+
+const booksPath = path.join(__dirname, "../../data/books.json")
+
+const readBooks = async() => {
+    const buffer = await fs.readFile(booksPath)
+    return JSON.parse(buffer.toString())
+}
+
+const writeBooks = async(data) => {
+    await fs.writeFile(booksPath, JSON.stringify(data))
+}
+
+router.get("/", async(req,res) => {
+    return res.send(await readBooks())
+})
+
+router.get("/:asin", async(req, res) => {
+    const books = await readBooks()
+    const book = books.find(book => book.asin === req.params.asin)
+    if(book) return res.status(200).send(book)
+    else return res.status(404).send("Non found")
+})
+
+router.post("/",[
+    check("asin")
+        .exists().withMessage("Asin is required"),
+    check("title")
+        .isLength({min:4,max:50}).withMessage("Title is required"),
+    check("price")
+        .isNumeric().withMessage("Price should be a number"),
+    check("category")
+        .exists().withMessage("Category is required"),
+    sanitizeBody("price").toFloat()
+    ],
+    async(req,res) => {
+        const books = await readBooks()
+        const obj = req.body
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) return res.status(400).send({errors: errors.array()})
+        books.push(obj)
+        await writeBooks(books)
+        return res.status(200).send("OK")
+})
+
+router.put("/:asin", async(req, res) => {
+    const books = await readBooks()
+    const book = books.find(book => book.asin === req.params.asin)
+    if(book){
+        const position = books.indexOf(book)
+        const updatedBook = Object.assign(book, req.body)
+        books[position] = updatedBook
+        await writeBooks(books)
+        res.status(200).send(updatedBook)
+    } else return res.status(404).send("Book not found")
+})
+
+router.delete("/:asin", async(req,res) => {
+    const books = await readBooks()
+    const filteredBooks = books.filter(book => book.asin !== req.params.asin)
+    if(filteredBooks.length === books.length) return res.status(404).send("Not found")
+    else {
+        await writeBooks(filteredBooks)
+        return res.status(200).send("Deleted")
+    }
+})
+
+module.exports = router
