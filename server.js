ac41e2e0e6b4d7b01b5974be0066ea56a5ec7de1
fainapahanko <fainapoganko@MacBook-Air-Faina.local>
2019-12-17 14:40:30 +0100

    +

--- server.js
+++ server.js
@@ -8,20 +8,10 @@ require('dotenv').config()
 const port = process.env.PORT || 4000
 
 server.use(express.json())
+server.use(cors())
 
-var whitelist = ['http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-server.use("/books", cors(corsOptions), booksRoute)
-server.use("/comments", cors(corsOptions), commentsRoute)
+server.use("/books", booksRoute)
+server.use("/comments", commentsRoute)
 
 server.get("/", () => {
     res.send("toyoy")
