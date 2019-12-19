feb2a343ffcf3368b7f323c11a2745fd606bc17a
fainapahanko <fainapoganko@MacBook-Air-Faina.local>
2019-12-19 13:51:39 +0100

    solving cors

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const port = process.env.PORT || 4400
 
 server.use(express.json({limit: '5000mb'}))
 server.use(cors())
-
+server.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+})
 server.use(express.static('./public/images'))
 server.use("/books", booksRoute)
 server.use("/comments", commentsRoute)
